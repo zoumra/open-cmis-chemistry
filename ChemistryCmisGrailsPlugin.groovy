ab9af797c533618b165db92cfbe7dd3ce65c0862
ludovik.lacroix@gmail.com <ludovik.lacroix@gmail.com>
2013-10-18 10:54:07 +0200

    plugin setup-1

--- ChemistryCmisGrailsPlugin.groovy
+++ ChemistryCmisGrailsPlugin.groovy
@@ -1,40 +1,40 @@
 class ChemistryCmisGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.3 > *"
+    def grailsVersion = "2.2.1 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
 
     // TODO Fill in these fields
-    def title = "Chemistry Cmis Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = "Apache Chemistry CMIS plugin for Grails" // Headline display name of the plugin
+    def author = "Ludovik Lacroix"
+    def authorEmail = "ludovik@lacroix.in"
     def description = '''\
 Apache Chemistry implementation for CMIS. Allows interaction with InMemoryRepository and Alfresco repositories.
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/chemistry-cmis"
+    def documentation = "https://github.com/llax/chemistry-cmis/blob/master/README.md"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-    def organization = [ name: "L2M", url: "http://www.lacroix.in" ]
+    //def organization = [ name: "L2M", url: "http://www.lacroix.in" ]
 
     // Any additional developers beyond the author specified above.
-    def developers = [ [ name: "Ludovik Lacroix", email: "ludovik@lacroix.in" ]]
+    //def developers = [ [ name: "llax", email: "ludovik@lacroix.in" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/llax/chemistry-cmis" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before

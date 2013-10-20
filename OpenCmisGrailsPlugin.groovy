d5d4e982c53c311fb25395ad68a27a6d1dae9b3d
Burt Beckwith <burt@burtbeckwith.com>
2013-10-20 11:11:36 -0400

    dropped minimum Grails version to 2.0

--- OpenCmisGrailsPlugin.groovy
+++ OpenCmisGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class OpencmisGrailsPlugin {
     // the plugin version
     def version = "0.10.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.3.0 > *"
+    def grailsVersion = "2.0.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
